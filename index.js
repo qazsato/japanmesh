43d8f1020e48d5810c3399b1cbf04104cca0a488
qazsato <qazsato@gmail.com>
2020-08-27 22:10:01 +0900

    add japanmesh

---
+++ index.js
@@ -0,0 +1,18 @@
+class japanmesh {
+  static toCode() {
+    // TODO
+    console.log('toCode')
+  }
+
+  static toPoint() {
+    // TODO
+    console.log('toPoint')
+  }
+
+  static toGeoJSON() {
+    // TODO
+    console.log('toGeoJSON')
+  }
+}
+
+module.export = japanmesh
