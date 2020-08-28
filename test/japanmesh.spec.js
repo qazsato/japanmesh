d94d4fa239be6e8416be0cd6bf68267912152e53
qazsato <qazsato@gmail.com>
2020-08-28 22:15:53 +0900

    add jest

---
+++ test/japanmesh.spec.js
@@ -0,0 +1,31 @@
+const japanmesh = require('./../index')
+const LEVEL1_CODES = require('./../src/constants/level1_codes')
+
+test('japanmesh.toCode', () => {
+  expect(japanmesh.toCode(35.70078, 139.71475)).toBe('5339454711')
+})
+
+test('japanmesh.toGeoJSON', () => {
+  const geojson = {
+    geometry: {
+      coordinates: [
+        [
+          [139, 36.666666666666664],
+          [138, 36.666666666666664],
+          [138, 36],
+          [139, 36],
+          [139, 36.666666666666664],
+        ],
+      ],
+      type: 'Polygon',
+    },
+    properties: {},
+    type: 'Feature',
+  }
+  expect(japanmesh.toGeoJSON('5438')).toEqual(geojson)
+})
+
+test('japanmesh.getCodes', () => {
+  const codes = japanmesh.getCodes()
+  expect(codes).toBe(LEVEL1_CODES)
+})
