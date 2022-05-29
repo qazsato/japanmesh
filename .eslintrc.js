eebaa2041df45602da7fcf136352c054b4c06cfc
qazsato <qazsato@gmail.com>
2022-05-29 11:14:13 +0900

    add requiring-type-checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
+    project: './tsconfig.json',
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
     'plugin:prettier/recommended',
   ],
